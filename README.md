269dd0c086c10d39cb69cabb30840a9b54adc0eb
= <=>
2019-11-05 11:54:13 +0800

    remove func done in mypc

--- README.md
+++ README.md
@@ -6,3 +6,6 @@
 - [ ] show up some terrifying pic suddenly
 - [ ] play horable music sometimes
 - [ ] make your windows flow
+
+### still consider
+- [ ] show up fake virus error
--- ghost.py
+++ ghost.py
@@ -3,4 +3,25 @@ import pyautogui
 pyautogui.position()
 
 
-pyautogui.moveTo(100,100,2)
+#pyautogui.moveTo(100,100,2)
+
+
+#button7location = pyautogui.locateOnScreen('calc7key.png')
+#print(button7location[0])
+
+try:
+	rebtn = pyautogui.locateOnScreen('img/close.PNG',confidence=0.9)
+
+	x = rebtn[0]
+	y = rebtn[1]
+
+	pyautogui.moveTo(x,y,2)
+
+	pyautogui.click()
+except:
+	
+	print("no remove button found")
+
+
+
+#rebtn = pyautogui.locateOnScreen('img/close.PNG',confidence=0.9)
